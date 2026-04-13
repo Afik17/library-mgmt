e868423d2e890893506b03fa1f7f8c8177f50938
afik17 <afiystudy@gmail.comD>
2026-04-13 13:42:17 +0300

    feat: mongodb configuration pass via env vars

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,25 @@
 from functools import lru_cache
 
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class MongoDBSettings(BaseModel):
+    uri: str
+    port: int
+    username: str
+    password: str
+    db: str
 
 
 class Settings(BaseSettings):
     overdue_return_fine: float
     transactions_file_path: str
+    mongodb: MongoDBSettings
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", env_nested_delimiter="__"
+    )
 
 
 @lru_cache
