f208fbac1fff2f652e15b8c60ea6d28756d2351d
afik17 <afiystudy@gmail.comD>
2026-04-19 15:27:42 +0300

    feat: convert methods to async

--- src/services/inventory.py
+++ src/services/inventory.py
@@ -27,20 +27,20 @@ class InventoryManager:
     borrow_repo: Repository[Borrow]
     logger: TransactionLogger
 
-    def add_book(self, book: BookCreate) -> Book:
+    async def add_book(self, book: BookCreate) -> Book:
         book_id = str(uuid.uuid4())[:8]
         new_book = Book(book_id=book_id, **book.model_dump())
-        self.book_repo.save(entity=new_book)
+        await self.book_repo.save(entity=new_book)
         self.logger.record_transaction(
             action=TransactionActions.ADD_BOOK, book_id=book_id
         )
         return new_book
 
-    def update_book(self, book_id: str, book: BookUpdate) -> Book:
-        if not self.get_book_by_id(book_id=book_id):
+    async def update_book(self, book_id: str, book: BookUpdate) -> Book:
+        if not await self.get_book_by_id(book_id=book_id):
             raise BookNotFound(book_id=book_id)
         updated_fields = book.model_dump(exclude_unset=True)
-        updated_book: Book = self.book_repo.update(
+        updated_book: Book = await self.book_repo.update(
             entity_id=book_id, updated_fields=updated_fields
         )
         self.logger.record_transaction(
@@ -48,36 +48,35 @@ class InventoryManager:
         )
         return updated_book
 
-    def remove_book(self, book_id: str) -> None:
-        if not self.get_book_by_id(book_id=book_id):
+    async def remove_book(self, book_id: str) -> None:
+        if not await self.get_book_by_id(book_id=book_id):
             raise BookNotFound(book_id=book_id)
-        self.book_repo.remove(entity_id=book_id)
+        await self.book_repo.remove(entity_id=book_id)
         self.logger.record_transaction(
             action=TransactionActions.REMOVE_BOOK, book_id=book_id
         )
 
-    def get_book_by_id(self, book_id: str) -> Optional[Book]:
-        return self.book_repo.get_by_id(entity_id=book_id)
+    async def get_book_by_id(self, book_id: str) -> Optional[Book]:
+        return await self.book_repo.get_by_id(entity_id=book_id)
 
-    def search_books(self, criteria: BookSearchCriteria) -> list[Book]:
+    async def search_books(self, criteria: BookSearchCriteria) -> list[Book]:
         search_criteria = criteria.model_dump(exclude_none=True)
-        return self.book_repo.find_by_criteria(**search_criteria)
+        return await self.book_repo.find_by_criteria(**search_criteria)
 
-    def get_borrow_by_book(self, book_id: str) -> Borrow:
+    async def get_borrow_by_book(self, book_id: str) -> Borrow:
         try:
-            return self.borrow_repo.find_by_criteria(book_id=book_id)[0]
+            return await self.borrow_repo.find_by_criteria(book_id=book_id)[0]
         except IndexError:
             raise BookNotBorrowed(book_id=book_id)
-        return self.borrow_repo.find_by_criteria(book_id=book_id)[0]
 
-    def borrow_book(self, book_id: str, book_borrow: BookBorrow) -> Borrow:
-        book: Book = self.get_book_by_id(book_id=book_id)
+    async def borrow_book(self, book_id: str, book_borrow: BookBorrow) -> Borrow:
+        book: Book = await self.get_book_by_id(book_id=book_id)
         if not book:
             raise BookNotFound(book_id=book_id)
         if book.status != BookStatus.AVAILABLE:
             raise BookUnavailable(book_id=book_id)
         book.status = BookStatus.BUSY
-        self.update_book(book_id=book_id, book=BookUpdate(**asdict(book)))
+        await self.update_book(book_id=book_id, book=BookUpdate(**asdict(book)))
         borrow_id = str(uuid.uuid4())[:8]
         new_borrow = Borrow(
             borrow_id=borrow_id,
@@ -85,7 +84,7 @@ class InventoryManager:
             return_date=None,
             **book_borrow.model_dump(),
         )
-        self.borrow_repo.save(entity=new_borrow)
+        await self.borrow_repo.save(entity=new_borrow)
         self.logger.record_transaction(
             action=TransactionActions.RETURN_BOOK,
             book_id=book_id,
@@ -94,31 +93,31 @@ class InventoryManager:
         )
         return new_borrow
 
-    def extend_borrow_book(self, book_id: str, days: int) -> Borrow:
-        book: Book = self.get_book_by_id(book_id=book_id)
+    async def extend_borrow_book(self, book_id: str, days: int) -> Borrow:
+        book: Book = await self.get_book_by_id(book_id=book_id)
         if not book:
             raise BookNotFound(book_id=book_id)
-        borrow: Borrow = self.get_borrow_by_book(book_id=book_id)
+        borrow: Borrow = await self.get_borrow_by_book(book_id=book_id)
         if not borrow or book.status == BookStatus.AVAILABLE:
             raise BookNotBorrowed(book_id=book_id)
         new_due_date = borrow.due_date + timedelta(days=days)
-        updated_borrow = self.borrow_repo.update(
+        updated_borrow = await self.borrow_repo.update(
             entity_id=borrow.borrow_id, updated_fields={"due_date": new_due_date}
         )
         return updated_borrow
 
-    def return_book(self, book_id: str) -> Borrow:
-        book: Book = self.get_book_by_id(book_id=book_id)
+    async def return_book(self, book_id: str) -> Borrow:
+        book: Book = await self.get_book_by_id(book_id=book_id)
         if not book:
             raise BookNotFound(book_id=book_id)
-        borrow: Borrow = self.get_borrow_by_book(book_id=book_id)
+        borrow: Borrow = await self.get_borrow_by_book(book_id=book_id)
         if book.status != BookStatus.BUSY:
             raise BookNotBorrowed(book_id=book_id)
-        returned_borrow = self.borrow_repo.update(
+        returned_borrow = await self.borrow_repo.update(
             entity_id=borrow.borrow_id, updated_fields={"return_date": datetime.now()}
         )
         book.status = BookStatus.AVAILABLE
-        self.update_book(book_id=book_id, book=BookUpdate(**asdict(book)))
+        await self.update_book(book_id=book_id, book=BookUpdate(**asdict(book)))
         self.logger.record_transaction(
             action=TransactionActions.RETURN_BOOK,
             book_id=borrow.book_id,
